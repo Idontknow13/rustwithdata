2a92e2ac44b5b1e6191cfe737fc1a232beb6c751
Idontknow13 <pokeminecraftexpert@gmail.com>
2023-05-28 19:53:44 +0800

    Added logging

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use anyhow::{Context, Result};
 use axum::{routing::get, Router};
 
+/// A basic "*hello*" GET endpoint.
 async fn index() -> &'static str {
     "Hello, RustWithData!"
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // enable tracing by default
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::TRACE)
+        .init();
+
     let app = Router::new().route("/", get(index));
 
     let addr = "[::]:8080";
