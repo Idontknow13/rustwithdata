92a5ae526cc847a6b961fe6b0024d50b8f8d0850
Idontknow13 <pokeminecraftexpert@gmail.com>
2023-05-28 19:42:38 +0800

    Added index endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Context, Result};
+use axum::{routing::get, Router};
+
+async fn index() -> &'static str {
+    "Hello, RustWithData!"
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let app = Router::new().route("/", get(index));
+
+    let addr = "[::]:8080";
+    axum::Server::try_bind(&addr.parse()?)
+        .context(format!("Failed to bind to address `{addr}`"))?
+        .serve(app.into_make_service())
+        .await?;
+    Ok(())
 }
