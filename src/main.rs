5a0cc56f6d6e204f3be175a1a6a19196c8440b80
Idontknow13 <pokeminecraftexpert@gmail.com>
2023-05-28 20:17:55 +0800

    Added Postgres connection

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 mod schema;
 
+use std::time::Duration;
+
 use anyhow::{Context, Result};
 use axum::{routing::get, Router};
+use sqlx::postgres::PgPoolOptions;
 
 /// A basic "*hello*" GET endpoint.
 async fn index() -> &'static str {
@@ -15,7 +18,18 @@ async fn main() -> Result<()> {
         .with_max_level(tracing::Level::TRACE)
         .init();
 
-    let app = Router::new().route("/", get(index));
+    // Creating the database connection pool with config from `axum/examples/sqlx-postgres`
+    let connstr = "postgresql://postgres:password@rustwithdata_pg/userdb".to_string();
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .acquire_timeout(Duration::from_secs(3))
+        .connect(&connstr)
+        .await
+        .context("Failed to connect to database")?;
+
+    let app = Router::new()
+        .route("/", get(index))
+        .nest("/users", Router::new().with_state(pool));
 
     let addr = "[::]:8080";
     axum::Server::try_bind(&addr.parse()?)
